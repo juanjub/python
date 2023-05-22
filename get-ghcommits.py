1015205a711f9782e306add73657c335b08c9b1a
juanjub <133943825+juanjub@users.noreply.github.com>
2023-05-21 21:02:26 -0600

    Update get-ghcommits.py

--- get-ghcommits.py
+++ get-ghcommits.py
@@ -6,7 +6,7 @@ from datetime import datetime
 # GitHub API URL for searching commits
 API_URL = 'https://api.github.com/search/commits'
 
-def fetch_popular_commits(start_date, end_date=date.today().isoformat(), export_type='mysql', popularity_metric='stars', min_popularity=100, language='', auth_token=''):
+def get_popular_commits(start_date, end_date=date.today().isoformat(), export_type='mysql', popularity_metric='stars', min_popularity=100, language='', auth_token=''):
     # Setup query parameters for the API
     params = {
         'q': f'{popularity_metric}:{min_popularity}',
@@ -177,4 +177,4 @@ def log_info(log_message, error=False):
     with open('script_log.txt', 'a') as log_file:
         log_file.write(log_entry)
 
-fetch_popular_commits()
+get_popular_commits()
